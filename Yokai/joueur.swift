449800c5559b636fa6436445fd2b9903fbb51953
RemiSalmi <33497248+RemiSalmi@users.noreply.github.com>
2018-11-27 18:35:20 +0100

    Add files via upload

---
+++ Yokai/joueur.swift
@@ -0,0 +1,31 @@
+//Un joueur contient un numéro, des Yokais sur plateau et une réserve de Yokais vide
+
+protocol joueurProtocol{
+  associatedtype yokaisIterator : IteratorProtocol
+  associatedtype reserveIterator : IteratorProtocol
+  //init : Int -> joueur
+  //#Création d'un joueur
+  //#Pré un joueur commence avec 4 Yokais sur le plateau (1 Koropokkuru, 1 Kitsune, 1 Tanuki, 1Kodama) et sa réserve est vide
+  //#données NumJoueur est un int qui prend seulement les valeurs 0 ou 1
+  init(NumJoueur : Int)
+
+  //estGagnant : joueur -> Bool
+  //#Vérfie si un joueur viens de gagner la parti pendant le tour en cours
+  //#Pré Le joueur doit avoir été créé
+  func estGagnant(Joueur : joueur)->Bool
+
+  //getYokais : joueur -> String
+  //Renvoie les noms et les numéros de tous les Yokais du plateau du joueur (exemple Kodama1;Tanuki1;...)
+  func getYokais(Joueur : joueur)->yokaisIterator
+
+  //getReserve : joueur -> String
+  //Renvoie les noms et les numéros de tous les Yokais en réserve du joueur
+  func getReserve(Joueur : joueur)->reserveIterator
+
+  //ajoutReserve : Yokais
+  //
+
+
+
+
+}
