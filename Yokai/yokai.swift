e09e26d7c58c0d4e63d96732e7823ace3a64669a
GAzerty <guilherme.t14@gmail.com>
2018-11-29 23:55:25 +0100

    ajout du protocol pour yokai

---
+++ Yokai/yokai.swift
@@ -0,0 +1,9 @@
+//Un yokai possède un type des directions et une position.
+//Il existe 5 types de yokais (Koropokkuru, Kitsune, Tanuki, Kodama, Kodoma Samurai)
+//Chaque type de yokais possède des directions différentes allant de 1 à 8
+//Un yokai est en réserve lorsqu’il n’a pas de position , c’est à dire quand position == nil
+
+protocol yokaiProtocol {
+
+
+}
