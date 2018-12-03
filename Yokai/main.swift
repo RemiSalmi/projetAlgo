efc0b120b3b178a5998b9bc94319405f23dd852a
GAzerty <guilherme.t14@gmail.com>
2018-12-03 09:15:58 +0100

    implémentation du main

--- Yokai/main.swift
+++ Yokai/main.swift
@@ -18,22 +18,42 @@ while !Jeu.getJoueur(jeu: Jeu, idjoueur: 1).estGagnant() and !Jeu.getJoueur(jeu:
   //Tester avec readLine()
 
   //Si choix 1 :
-      //Affichage des Yokai disponibles
-      //On recupère le choix du joueur
-      //On verifie que le Yokai choisit appartient au joueur
-      //On affiche les actions disponibles pour le Yokai
-      //On recupère le choix du joueur
-      //On verifie que l'action choisit correspond bien aux actions disponibles du Yokai en question
-      //On réalise le déplacement
+      IteratorMain = joueur.getMain(joueur: joueur)
+      for i in 0..<joueur.tailleMain(){//Affichage des Yokai disponibles
+          print("Nom du Yokai:",IteratorMain.nom,"Id du Yokai:",IteratorMain.id)
+          IteratorMain.next()
+      }
+      //A faire: On recupère le choix du joueur
+      if joueur.estEnMain(j: joueur, y: yokai){ //On verifie que le Yokai choisit appartient au joueur
+          IteratorDeplacement = yokai.makeItDeplacement(y: yokai)//On affiche les actions disponibles pour le Yokai
+          for i in 0..<tailleDeplacement{
+            print("Deplacement:",IteratorDeplacement.nom)
+          }
+          //A faire: On recupère le choix du joueur
+        if peutAller(y: yokai, coord_x: x, coord_y: y){ //On verifie que l'action choisit correspond bien aux actions disponibles du Yokai en question
+          yokai.deplacer(y: yokai, p: positon)
+        }
+      }
 
   //Si choix 2 :
-      //Affichage des Yokai en réserve
-      //On recupère le choix du joueur
-      //On verifie que le Yokai choisit appartient à la réserve du joueur
-      //On affiche les cases libre du jeu
-      //On recupère la case souhaitée
-      //On verifie que la case choisie est libre
-      //On réalise le parachutage
+      IteratorReserve = joueur.getReserve(joueur: joueur)//On affiche les actions disponibles pour le Yokai
+      for i in 0..<tailleDeplacement{
+        print("Deplacement:",IteratorDeplacement.nom)
+      }
+      //A faire: On recupère le choix du joueur
+      if joueur.estEnReserve(j: joueur, y : yokai){//On verifie que le Yokai choisit appartient à la réserve du joueur
+
+        IteratorPlateau = Jeu.makeItPlateau(jeu: Jeu)//On affiche les cases libre du jeu
+        for i in 0..<12{
+          if !IteratorPlateau.est_Occupe(p: IteratorPlateau){
+            print("Position: x=",IteratorPlateau.get_CoordX,"y=",IteratorPlateau.get_CoordY)
+          }
+        }
+        //A faire: On recupère la case souhaitée
+        if !est_Occupe(p: positon){
+          joueur.parachuter(j: joueur, y: Yokai, p: position)
+        }
+      }
 
   //FinSi
   joueur = Jeu.getJoueurSuivant(jeu: Jeu, joueur:joueur)
