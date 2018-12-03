69b2ba13222b54b90639ba67438cd72827c739f0
RemiSalmi <remi.salmi@gmail.com>
2018-12-03 08:12:03 +0100

    fix

--- Yokai/position.swift
+++ Yokai/position.swift
@@ -6,44 +6,44 @@ var coord_y : Int
 //Attribut booleen, true si la position est occupé par un Yokai, false sinon
 var occuper : Bool
 
-//init: Int x Int -> PositonProtocol
+//init: Int x Int -> Positon
 //Creation d'une position à partir deux entiers
 //Pre :  0 <= x <= 2 , 0 <= y <= 3
 //Post : occuper = false
 init(coord_x : Int, coord_y : Int)
 
-//get_CoordX : PositonProtocol -> Int
+//get_CoordX : Positon -> Int
 //Retourne la coord_x de la positon donnée paramètre
-func get_CoordX(p : PositonProtocol)-> Int
+func get_CoordX(p : Positon)-> Int
 
-//get_CoordY : PositonProtocol -> Int
+//get_CoordY : Positon -> Int
 //Retourne la coord_y de la positon donnée paramètre
-func get_CoordY(p : PositonProtocol)-> Int
+func get_CoordY(p : Positon)-> Int
 
-//est_Occupe : PositonProtocol -> Bool
+//est_Occupe : Positon -> Bool
 //Retourne True si la position donnée est occupé
-func est_Occupe(p : PositonProtocol)->Int
+func est_Occupe(p : Positon)->Int
 
-//changeX : PositonProtocol  x Int -> PositonProtocol
+//changeX : Positon  x Int -> Positon
 // Change la valeur de coord_x de la positon passé en paramètre
 //Pré : La valeur du int passé en paramètre doit correspondre a une coord_x valide
-mutating func changeX(p : PositonProtocol, x : Int)-> PositonProtocol
+mutating func changeX(p : Positon, x : Int)-> Positon
 
-//changeY : PositonProtocol  x Int -> PositonProtocol
+//changeY : Positon  x Int -> Positon
 // Change la valeur de coord_y de la positon passé en paramètre
 //Pré : La valeur du int passé en paramètre doit correspondre a une coord_y valide
-mutating func changeY(p : PositonProtocol, y : Int)-> PositonProtocol
+mutating func changeY(p : Positon, y : Int)-> Positon
 
-//occuper : PositonProtocol -> PositonProtocol
+//occuper : Positon -> Positon
 //Passe la valeur de occuper de la position passé en paramètre à true
-mutating func occuper(p : PositonProtocol)-> PositonProtocol
+mutating func occuper(p : Positon)-> Positon
 
-//liberer : PositonProtocol -> PositonProtocol
+//liberer : Positon -> Positon
 //Passe la valeur de occuper de la position passé en paramètre à false
-mutating func liberer(p : PositonProtocol)-> PositonProtocol
+mutating func liberer(p : Positon)-> Positon
 
-//setPosition : PositonProtocol x PositonProtocol -> PositonProtocol
+//setPosition : Positon x Positon -> Positon
 //change les valeurs coord_x et coord_y de la premiere position passe en parametre par celles de la seconde
-mutating func setPosition(p1 : PositonProtocol, p2 : PositonProtocol)-> PositonProtocol
+mutating func setPosition(p1 : Positon, p2 : Positon)-> Positon
 
 }
