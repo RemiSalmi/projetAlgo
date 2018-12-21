c9fefd2408184a8841e78949426aa847ef90cbbe
GAzerty <guilherme.t14@gmail.com>
2018-12-22 00:13:19 +0100

    ajout de fonctions pour le type Main

--- Art_of_war/jeu/Sources/jeu/main.swift
+++ Art_of_war/jeu/Sources/jeu/main.swift
@@ -9,9 +9,55 @@ public struct main : mainProtocol {
   // Post : on crée une carte "roi1" ou "roi2"
   init(n:Int){
     let typeRoi = "roi"+String(n)
-    var roi = Carte(type: typeRoi)
-    if var roi = roi {
+    let roi = Carte(type: typeRoi)
+    if let roi = roi {
       self.main.append(roi)
     }
+  }
+
+  // mainVide : Main -> Bool
+  // indique si la main est vide ou pas
+  // Post : retourne True si main vide false sinon
+  func mainVide()->Bool{
+    return self.main.isEmpty
+  }
+
+  // tailleMain : Main-> Int
+  // indique le nombre de cartes dans la main
+  // Post : si la main est vide renvoyer 0
+  func tailleMain()->Int{
+    var taille : Int = 0
+    if !self.mainVide() {
+      taille = self.main.count
+    }
+    return taille
+  }
+
+  // ajouterCarteMain :  Main x Carte -> Main
+  // ajoute UNE seule carte dans une main
+  // Pre : la carte doit provenir de la pioche ???
+  // Post :tailleMain=tailleMain+1
+  @discardableResult
+  mutating func ajouterCarteMain(c:Carte)->main{
+    self.main.append(c)
+    return self
+  }
+
+  // setCarte :  Main x Int -> Carte
+  // renvoie la carte de la main à la position pos
+  // Pre : la position doit etre entre 1 et tailleMain()
+  //** Que faire si la pre condition n'est pas respectée ? **
+  func setCarte(pos:Int)->Carte{
+    return self.main[pos]
+  }
+
+  // supprimerCarte :  Main x Int -> Carte
+  // renvoie la carte de la main à la position pos et la supprime de la main
+  // Pre : la position doit etre entre 1 et tailleMain()
+  @discardableResult
+  mutating func supprimerCarte(pos:Int)->Carte{
+    var carte = self.setCarte(pos: pos)
+    self.main.remove(at: pos)
+    return carte
   }
 }
