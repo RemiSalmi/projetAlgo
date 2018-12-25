4a3b473179cbdd03fac436a349a30c2d8c1f0776
GAzerty <guilherme.t14@gmail.com>
2018-12-25 19:44:29 +0100

    Iterator sur le champ de bataille

--- Art_of_war/jeu/Sources/jeu/champBataille.swift
+++ Art_of_war/jeu/Sources/jeu/champBataille.swift
@@ -79,5 +79,32 @@ public struct champBataille : champBatailleProtocol {
     return carte
   }
 
+  func makeIt()->IteratorChampBataille{
+    return IteratorChampBataille(self)
+  }
+}
 
+public struct IteratorChampBataille : IteratorProtocol{
+  private let champ_bataille : champBataille
+  private var courant : Int = 0
+  fileprivate init(_ cb: champBataille){
+    self.champ_bataille = cb
+  }
+
+  //Note developpeurs:
+  //Tant que c'est vide :
+  //  On verifie si c'est une carte ou vide
+  // Si c'est une carte , c'est bon
+  // Si c'est vide, on passe au next
+  // Si on est a la fin, on retourne nil
+  public mutating func next()->Carte?{
+    guard self.courant < 6 else { return nil }
+
+    guard let carte = self.champ_bataille.champ[self.courant] else {
+      self.courant = self.courant + 1
+      return self.next()
+    }
+    self.courant = self.courant + 1
+    return carte
+  }
 }
