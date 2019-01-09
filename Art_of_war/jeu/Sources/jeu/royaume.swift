f4caeda4ac0d8c6411433b27d9a1abf53b404386
GAzerty <guilherme.t14@gmail.com>
2019-01-09 12:57:31 +0100

    Royaume en class

--- Art_of_war/jeu/Sources/jeu/royaume.swift
+++ Art_of_war/jeu/Sources/jeu/royaume.swift
@@ -1,4 +1,4 @@
-public struct royaume : royaumeProtocol {
+public class royaume : royaumeProtocol {
   public typealias Royaume = royaume
   public typealias Carte = carte
   var royaume : [Carte]
@@ -7,22 +7,22 @@ public struct royaume : royaumeProtocol {
   // init :  -> Royaume
   // Création d'un royaume initialisé à vide
   // Note : le royaume est une collection de Carte géré comme une file
-  public init(){
+  public required init(){
     self.royaume = [Carte]()
   }
 
   // ajouterCarte : Royaume x Carte -> Royaume
   // Ajout d'une carte dans le royaume
   // Note : la carte est ajoutée à la fin du royaume (FILE)
-  
-  public mutating func ajouterCarte(c:Carte){
+
+  public func ajouterCarte(c:Carte){
     self.royaume.append(c)
   }
 
   // recupererCarte : Royaume -> Carte
   // Retirer une carte du royaume la premiere de la file
   @discardableResult
-  public mutating func recupererCarte()->Carte{
+  public func recupererCarte()->Carte{
     let c = self.royaume[0]
     self.royaume.removeFirst()
     return c
